6461d93d90d25fb9ba4e2c763c330a0e4fecddbf
chunchun730 <ccwkeep@gmail.com>
2016-11-17 13:08:49 -0500

    SQL vs RDD

--- Preaggregate.py
+++ Preaggregate.py
@@ -2,6 +2,9 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 import time
 from datetime import datetime
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 conf = SparkConf().setAppName("Pre-agregate raw files").setMaster("local").set("spark.eventLog.enabled", "true")
 sc = SparkContext(conf=conf)
@@ -15,29 +18,34 @@ files = sc.textFile("/home/chunchun/Documents/ICEWS/1995/*.tab")
 def read_line(line):
     return line.split("\t")
 
+def write_line(row):
+    return ",".join(map(encode_ascii, row[0]+(str(row[1]),)))
+
+def encode_ascii(row):
+    return row.encode('ascii', 'ignore')
+
 def keep_columns(line):
     if line[1] != "Event Date":
-        return (datetime.strptime(line[1], "%Y-%m-%d").strftime("%Y-%m"), line[4], line[10], line[6])
-    #return (line[1], line[4], line[10], line[6])
-    #return (datetime.strptime(line[1], "%Y-%m-%d").strftime("%Y-%m"))
+        source = line[4] if len(line[4]) > 0 else "Unknown"
+        target = line[10] if len(line[10]) > 0 else "Unknown"
+        return (datetime.strptime(line[1], "%Y-%m-%d").strftime("%Y-%m"), source, target, line[6])
+    return ('File count',)
 
 def print_f(line):
     print line
-
-def by_month(month):
+def by_month():
 
     t0 = time.time()
 
     extracted_files = files.map(read_line).map(keep_columns).coalesce(8)
     #{(YYYY-MM, Source, Target, CAMEO):count}
-    mapping = extracted_files.countByValue()
-
+    mapping = extracted_files.countByValue().items()
+    output = sc.parallelize(mapping).map(write_line).saveAsTextFile("/home/chunchun/Documents/ICEWS/preagg")
 
     t1 = time.time()
 
-    print mapping
     print "finished in ",t1-t0,' seconds.'
 
-by_month(1)
+by_month()
 
 
--- TestSQL.py
+++ TestSQL.py
@@ -1,49 +1,43 @@
 from pyspark.sql import SparkSession, SQLContext, Row
+from pyspark.sql.functions import *
 from pyspark import SparkContext, SparkConf
 import time
+from datetime import datetime
 
-spark = SparkSession.builder.appName("TestSQL").master("local").config("spark.eventLog.enabled", "true").getOrCreate()
 
-sc = spark.sparkContext
-sqlContext = SQLContext(sc)
-files = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='false').load('/home/chunchun/Documents/ICEWS/1995/*.tab')
-#files = sc.textFile("/home/chunchun/Documents/ICEWS/1995/*.tab")
+def extract(row):
+    date = datetime.strptime(row["Event Date"], "%Y-%m-%d").strftime("%Y-%m")
+    return ((date, row["Source Country"], row["Target Country"], row["CAMEO Code"]), 1)
+
+def append_res(line):
+    return line[0]+(line[1],)
+
+conf = SparkConf().setAppName("TestSQL").setMaster("local").set("spark.eventLog.enabled", "true")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
 t0 = time.time()
-lines = files.map(lambda l: l.split("\t"))
-interactions = lines.map(lambda p: Row(source=p[4], to=p[10], cameo=p[6]))
+files = spark.read.options(header="true", delimiter="\t").csv("/home/chunchun/Documents/ICEWS/1995/*.tab")
+
+#files = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='false').load('/home/chunchun/Documents/ICEWS/1995/*.tab')
+#files = sc.textFile("/home/chunchun/Documents/ICEWS/1995/*.tab")
+
+
+#print files.dtypes
+#files.select(col("Event Date").alias("EventDate"))
+files_rdd = files.rdd
+res = files_rdd.map(extract).coalesce(3).countByKey().items()
+ans = sc.parallelize(res).map(append_res)
+df = spark.createDataFrame(ans, ["Date", "Source", "Target", "CAMEO", "Count"])
+df.write.csv("/home/chunchun/Documents/ICEWS/preagg-sql")
 t1 = time.time()
+#print files.columns
+#print ans
 print "finished in ",t1-t0,' seconds.'
 
-sql = spark.createDataFrame(interactions)
-
-def read_line(line):
-    return line.split("\t")
-
-def keep_columns(line):
-    if line[1] != "Event Date":
-        return (datetime.strptime(line[1], "%Y-%m-%d").strftime("%Y-%m"), line[4], line[10], line[6])
-    #return (line[1], line[4], line[10], line[6])
-    #return (datetime.strptime(line[1], "%Y-%m-%d").strftime("%Y-%m"))
-
-def print_f(line):
-    print line
-
-def by_month(month):
-
-    t0 = time.time()
-
-    extracted_files = files.map(read_line).map(keep_columns).coalesce(8)
-    #{(YYYY-MM, Source, Target, CAMEO):count}
-    mapping = extracted_files.countByValue()
 
 
-    t1 = time.time()
 
-    print mapping
-    print "finished in ",t1-t0,' seconds.'
-
-by_month(1)
 
 
 
