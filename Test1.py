1ff7db684b72fdb6faf0c2bcaf0f37a293c77f50
chunchun730 <ccwkeep@gmail.com>
2016-11-18 15:52:07 -0500

    adding object

--- Test1.py
+++ Test1.py
@@ -16,6 +16,9 @@ files = sc.textFile("/home/chunchun/Documents/ICEWS/preagg/*.csv")
 def print_f(line):
     print line
 
+def read_csv(line):
+    row = line.split(",")
+    return (tuple(row[:-1]), row[-1])
 
 def sum_events(fromdate=None, todate=None):
     """
@@ -45,14 +48,29 @@ def sum_events(fromdate=None, todate=None):
         except Exception:
             return False
 
-    extracted_files = files.map(read_line).coalesce(8)
+    extracted_files = files.map(read_line).coalesce(11)#finished in  7.88416600227  seconds. Use 1995-1998.csv
     mapping = extracted_files.collectAsMap()
 
     t1 = time.time()
 
     #print mapping
     print "finished in ",t1-t0,' seconds.'
-    #Event ID   Event Date  Source Name Source Sectors  Source Country  Event Text  CAMEO Code  Intensity   Target Name Target Sectors  Target Country  Story ID    Sentence Number Publisher   City    District    Province    Country Latitude    Longitude
 
-sum_events("1995-01", "2013-01")
+def load_pregg():
+    t0 = time.time()
+    mapping = files.map(read_csv).coalesce(5).collectAsMap()#finished in  3.72139501572  seconds. Use 1995-1998
+    t1 = time.time()
+    print "finished in ",t1-t0,' seconds.'
 
+def load_and_filter(fromdate=None, todate=None):
+    start_date = datetime.strptime(fromdate, "%Y-%m")
+    end_date = datetime.strptime(todate, "%Y-%m")
+
+    t0 = time.time()
+    mapping = files.map(read_csv).filter(lambda row: row[0][0] != "File count" and start_date <= datetime.strptime(row[0][0], '%Y-%m') <= end_date).coalesce(5).collectAsMap()#finished in  3.72139501572  seconds. Use 1995-1998
+    t1 = time.time()
+    print "finished in ",t1-t0,' seconds.'
+
+#sum_events("1995-01", "1996-01")
+#load_pregg()
+load_and_filter("1995-01", "1996-01")
