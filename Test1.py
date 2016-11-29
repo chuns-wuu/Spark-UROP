22b7216571045522716ccdeaa38b3d13d6414730
chunchun730 <ccwkeep@gmail.com>
2016-11-29 15:43:04 -0500

    Added object, overhead ~ 2.5 seconds. Next: add qclass + goldstein

--- Test1.py
+++ Test1.py
@@ -12,6 +12,21 @@ global mapping
 mapping = {}
 files = sc.textFile("/home/chunchun/Documents/ICEWS/preagg/*.csv")
 
+class Key:
+    def __init__(self, line):
+        self.row = line.split(",")
+        self.date_ = self.row[0]
+        self.date = datetime.strptime(self.row[0], "%Y-%m") if self.row[0] != 'File count' else datetime.today()
+        self.pair = self.row[1], self.row[2] if self.row[0] != 'File count' else "", ""
+        self.count = self.row[3] if self.row[0] != 'File count' else 0
+
+    def add_qclass(self, q):
+        self.q = q
+    def add_goldstein(self, g):
+        self.g = g
+
+    def get_qclass(self):
+        return {(self.date_, self.pair): self.q}
 
 def print_f(line):
     print line
@@ -19,6 +34,15 @@ def print_f(line):
 def read_csv(line):
     row = line.split(",")
     return (tuple(row[:-1]), row[-1])
+
+def create_keys(line):
+    key = Key(line)
+    key.add_qclass("Qclass")
+    key.add_goldstein("G")
+    return key
+
+def get_dic(key):
+    return key.get_qclass()
 
 def sum_events(fromdate=None, todate=None):
     """
@@ -67,10 +91,20 @@ def load_and_filter(fromdate=None, todate=None):
     end_date = datetime.strptime(todate, "%Y-%m")
 
     t0 = time.time()
-    mapping = files.map(read_csv).filter(lambda row: row[0][0] != "File count" and start_date <= datetime.strptime(row[0][0], '%Y-%m') <= end_date).coalesce(5).collectAsMap()#finished in  3.72139501572  seconds. Use 1995-1998
+    mapping = files.map(read_csv).filter(lambda row: row[0][0] != "File count" and start_date <= datetime.strptime(row[0][0], '%Y-%m') <= end_date).coalesce(5).collectAsMap()#finished in  7.60540795326  seconds. Use 1995-1998
     t1 = time.time()
     print "finished in ",t1-t0,' seconds.'
 
+def use_key(fromdate=None, todate=None):
+    start_date = datetime.strptime(fromdate, "%Y-%m")
+    end_date = datetime.strptime(todate, "%Y-%m")
+
+    t0 = time.time()
+    mapping = files.map(create_keys).filter(lambda key: start_date <= key.date <= end_date).map(get_dic).collect()
+    t1 = time.time()
+    print mapping
+    print "finished in ",t1-t0,' seconds.'
 #sum_events("1995-01", "1996-01")
 #load_pregg()
-load_and_filter("1995-01", "1996-01")
+#load_and_filter("1995-01", "1996-01")
+#use_key("1995-01", "1996-01")
